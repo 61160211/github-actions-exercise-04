bae1d0ced335090c5098fb1b86a466f2feca0c60
Tackdanai Suban <61160211@go.buu.ac.th>
2021-10-11 13:59:22 +0700

    github-actions-04

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello world!'})
+})
+
+app.listen(port, host, () => {
+    console.log('The app is up and running!')
+})
+
+module.exports = app
